86dc81df542cd4dae09f432321ad135ff1caad7d
crsib <crsib@localhost>
2010-05-06 08:59:17 +0000

    Closes #11. Needs testing and discussing of engine behavior when a plugin fails to load (currently, throws an exception and terminates the execution with correct destruction)

--- Framework/Source/core/EngineCore.cpp
+++ Framework/Source/core/EngineCore.cpp
@@ -60,15 +60,15 @@ using namespace renderer;
 
 namespace core
 {
-	EXPORT core::PluginCore*	CoreInstance;
-	//core::PluginCore*	CoreInstance;
+EXPORT core::PluginCore*	CoreInstance;
+//core::PluginCore*	CoreInstance;
 namespace memory {
 #ifdef _FREYA_DEBUG_MEMORY
-	extern unsigned memory_allocated;
-	extern unsigned allocation_count;
-	extern unsigned deallocation_count;
-	extern unsigned alloc_dealloc_dif;
-	extern unsigned allocated_for_buffers;
+extern unsigned memory_allocated;
+extern unsigned allocation_count;
+extern unsigned deallocation_count;
+extern unsigned alloc_dealloc_dif;
+extern unsigned allocated_for_buffers;
 #endif
 }
 core::PluginCore*							EngineCore::m_PluginCore  = NULL;
@@ -86,9 +86,9 @@ core::taskmanager::TaskManager*				EngineCore::m_TaskManager = NULL;
 resources::ResourceManager*					EngineCore::m_ResourceManager = NULL;
 
 THREAD_IMPLEMENTATION	*					EngineCore::m_ThreadImplementation = NULL;
-	
+
 core::PluginLoader*							EngineCore::m_PluginLoader         = NULL;
-	
+
 std::stringbuf*								EngineCore::m_LogStringBuf = NULL;
 
 core::lua::LuaCore*							EngineCore::m_LuaCore = NULL;
@@ -102,9 +102,9 @@ namespace __internal
 extern MemoryArena		arena_inst;
 }
 void* Allocate(size_t n,unsigned id)
-{
+						{
 	return __internal::arena_inst.allocate(n,id);
-}
+						}
 
 void Free(void* p,unsigned id)
 {
@@ -112,105 +112,183 @@ void Free(void* p,unsigned id)
 }
 
 void* Reallocate(void* p,size_t sz,unsigned id)
-{
+						{
 	//return EngineCore::getMemoryArena()->reallocate(p,sz);
 	return NULL;
-}
+						}
 }
 //Constructor and destructor
 EngineCore::EngineCore(int argC,char** argV,const std::string& applicationName, const std::string& applicationTeam, const std::string& configName )
 {
 	if(m_Instance)
 		throw EngineException();
-	settings::application_name 		= new char[applicationName.length()+1];
-	::memcpy(settings::application_name,applicationName.c_str(),applicationName.length() + 1);
-	settings::application_company 	= new char[applicationTeam.length()+1];
-	::memcpy(settings::application_company,applicationTeam.c_str(),applicationTeam.length() + 1);
-	settings::config_file_name		= new char[configName.length()+1];
-	::memcpy(settings::config_file_name,configName.c_str(),configName.length() + 1);
-
-	std::cout << "Starting log subsystem" << std::endl;
-	std::string path(argV[0]);
-	size_t slash_pos = path.find_last_of("/\\" );
-	if(slash_pos == std::string::npos)
-		path = std::string(settings::application_name) + ".log";
-	else
+	try
 	{
-		path.erase(slash_pos + 1);
+		settings::application_name 		= new char[applicationName.length()+1];
+		::memcpy(settings::application_name,applicationName.c_str(),applicationName.length() + 1);
+		settings::application_company 	= new char[applicationTeam.length()+1];
+		::memcpy(settings::application_company,applicationTeam.c_str(),applicationTeam.length() + 1);
+		settings::config_file_name		= new char[configName.length()+1];
+		::memcpy(settings::config_file_name,configName.c_str(),configName.length() + 1);
+
+		std::cout << "Starting log subsystem" << std::endl;
+		std::string path(argV[0]);
+		size_t slash_pos = path.find_last_of("/\\" );
+		if(slash_pos == std::string::npos)
+			path = std::string(settings::application_name) + ".log";
+		else
+		{
+			path.erase(slash_pos + 1);
 #ifdef __APPLE__
-		slash_pos = path.find_last_of("/\\" );
-		slash_pos = path.find_last_of("/\\",slash_pos - 1 );
-		slash_pos = path.find_last_of("/\\",slash_pos - 1 );
-		slash_pos = path.find_last_of("/\\",slash_pos - 1 );
-		path.erase(slash_pos + 1);
+			slash_pos = path.find_last_of("/\\" );
+			slash_pos = path.find_last_of("/\\",slash_pos - 1 );
+			slash_pos = path.find_last_of("/\\",slash_pos - 1 );
+			slash_pos = path.find_last_of("/\\",slash_pos - 1 );
+			path.erase(slash_pos + 1);
 #endif
-		path += std::string(settings::application_name) + ".log";
-		std::cout << path << std::endl;
+			path += std::string(settings::application_name) + ".log";
+			std::cout << path << std::endl;
+		}
+		m_LogStream = new std::ofstream(path.c_str());
+		m_LogStringBuf = new std::stringbuf;
+		m_OutBuffer = new __internal::freya_buf(std::cout.rdbuf(),m_LogStream->rdbuf(),m_LogStringBuf);
+		m_OldCLogStream = std::clog.rdbuf(m_OutBuffer);
+		m_OldCOutStream = std::cout.rdbuf(m_OutBuffer);
+		//Start memory
+		std::cout << "Initializing memory subsystem" << std::endl;
+		m_MemoryArena = core::memory::MemoryArena::instance();
+		m_ThreadImplementation = new THREAD_IMPLEMENTATION;
+		//start filesystem
+		std::cout << "Starting filesystem subsystem" << std::endl;
+		m_Filesystem  = new core::filesystem::FilesystemInternal();
+
+		//m_Settings  = new Settings();
+
+		std::cout << "Starting Lua scripting engine" << std::endl;
+		m_LuaCore 			= new core::lua::LuaCore;
+		m_Running 			= true;
+		m_Instance 			= this;
+		m_RenderingDriver 	= NULL;
+		m_WindowManager	  	= NULL;
+		std::cout << "Creating task manager" << std::endl;
+		m_TaskManager = new core::taskmanager::TaskManager;
+		std::cout << "Creating resource manager" << std::endl;
+		m_ResourceManager = new resources::ResourceManager;
+		std::cout << "Creating factories" << std::endl;
+		m_WMFactory	=	new windowmanager::WindowManagerFactory();
+		m_RAPIFactory = new renderer::RenderingAPIFactory();
+		m_PluginCore  = new core::PluginCoreInternal();
+		CoreInstance  = m_PluginCore;
+		m_PluginLoader = new core::PluginLoader(argC,argV);
 	}
-	m_LogStream = new std::ofstream(path.c_str());
-	m_LogStringBuf = new std::stringbuf;
-	m_OutBuffer = new __internal::freya_buf(std::cout.rdbuf(),m_LogStream->rdbuf(),m_LogStringBuf);
-	m_OldCLogStream = std::clog.rdbuf(m_OutBuffer);
-	m_OldCOutStream = std::cout.rdbuf(m_OutBuffer);
-	//Start memory
-	std::cout << "Initializing memory subsystem" << std::endl;
-	m_MemoryArena = core::memory::MemoryArena::instance();
-	m_ThreadImplementation = new THREAD_IMPLEMENTATION;
-	//start filesystem
-	std::cout << "Starting filesystem subsystem" << std::endl;
-	m_Filesystem  = new core::filesystem::FilesystemInternal();
+	catch(...)
+	{
+		std::cout << "Destroying task manager" << std::endl;
+		delete m_TaskManager;
+		m_TaskManager = NULL;
+		std::cout << "Stopping CEGUI" << std::endl;
+		CEGUI::System::destroy();
+		std::cout << "Destroying Lua engine " << std::endl;
+		delete m_LuaCore;
+		m_LuaCore = NULL;
+		std::cout << "Destroying resource manager" << std::endl;
+		delete m_ResourceManager;
+		m_ResourceManager = NULL;
+		std::cout << "Destroying rendering driver" << std::endl;
+		delete m_RenderingDriver;
+		m_RenderingDriver = NULL;
+		std::cout << "Destroying window manager" << std::endl;
+		delete m_WindowManager;
+		m_WindowManager = NULL;
+		delete m_RAPIFactory;
+		std::cout << "Destroying filesystem" << std::endl;
+		delete m_Filesystem;
+		m_Filesystem = NULL;
+		std::cout << "Destroying thread subsystem" << std::endl;
+		delete m_ThreadImplementation;
+		m_ThreadImplementation = NULL;
+		delete m_PluginCore;
+		m_PluginCore = NULL;
+		delete m_PluginLoader;
+		m_PluginLoader = NULL;
+		//std::cout << "Switching allocation mode" << std::endl;
+		//delete m_MemoryArena;
 
-	//m_Settings  = new Settings();
+#ifdef _FREYA_DEBUG_MEMORY
+		std::cout << "Engine memory usage at shutdown:\n"
+				<< "\tLeaked in internal manager: " << memory::memory_allocated / 1024.f << " Kb\n"
+				<< "\tTotal number of allocations " << memory::allocation_count << "\n"
+				<< "\tTotal number of deallocations: " << memory::deallocation_count << "\n"
+				<< "\tAlloc - Dealloc: " << (int) memory::alloc_dealloc_dif << "\n"
+				<< "\tSystem memory allocation for internal manager: " << memory::allocated_for_buffers / 1024.f / 1024.f << " Mb" << std::endl;
+		std::cout << "Engine shutdown completed" << std::endl;
+#endif
 
-	std::cout << "Starting Lua scripting engine" << std::endl;
-	m_LuaCore 			= new core::lua::LuaCore;
-	m_Running 			= true;
-	m_Instance 			= this;
-	m_RenderingDriver 	= NULL;
-	m_WindowManager	  	= NULL;
-	std::cout << "Creating task manager" << std::endl;
-	m_TaskManager = new core::taskmanager::TaskManager;
-	std::cout << "Creating resource manager" << std::endl;
-	m_ResourceManager = new resources::ResourceManager;
-	std::cout << "Creating factories" << std::endl;
-	m_WMFactory	=	new windowmanager::WindowManagerFactory();
-	m_RAPIFactory = new renderer::RenderingAPIFactory();
-	m_PluginCore  = new core::PluginCoreInternal();
-	CoreInstance  = m_PluginCore;
-	m_PluginLoader = new core::PluginLoader(argC,argV);
+		std::cout << "Stopping log subsystem" << std::endl;
+		std::clog.flush();
+		std::cout.flush();
+		std::clog.rdbuf(m_OldCLogStream);
+		std::cout.rdbuf(m_OldCOutStream);
+		delete m_OutBuffer;
+		m_OutBuffer = NULL;
+		delete m_LogStringBuf;
+		m_LogStringBuf = NULL;
+		m_LogStream->flush();
+		m_LogStream->close();
+		std::cout << "Destroying log" << std::endl;
+		delete m_LogStream;
+		m_LogStream = NULL;
+
+		delete [] settings::application_name;
+		settings::application_name = NULL;
+		delete [] settings::application_company;
+		settings::application_company = NULL;
+		delete [] settings::config_file_name;
+		settings::config_file_name = NULL;
+		throw;
+	}
 }
 
 EngineCore::~EngineCore()
 {
 	std::cout << "Destroying task manager" << std::endl;
 	delete m_TaskManager;
+	m_TaskManager = NULL;
 	std::cout << "Stopping CEGUI" << std::endl;
 	CEGUI::System::destroy();
 	std::cout << "Destroying Lua engine " << std::endl;
 	delete m_LuaCore;
+	m_LuaCore = NULL;
 	std::cout << "Destroying resource manager" << std::endl;
 	delete m_ResourceManager;
+	m_ResourceManager = NULL;
 	std::cout << "Destroying rendering driver" << std::endl;
 	delete m_RenderingDriver;
+	m_RenderingDriver = NULL;
 	std::cout << "Destroying window manager" << std::endl;
 	delete m_WindowManager;
+	m_WindowManager = NULL;
 	delete m_RAPIFactory;
 	std::cout << "Destroying filesystem" << std::endl;
 	delete m_Filesystem;
+	m_Filesystem = NULL;
 	std::cout << "Destroying thread subsystem" << std::endl;
 	delete m_ThreadImplementation;
+	m_ThreadImplementation = NULL;
 	delete m_PluginCore;
+	m_PluginCore = NULL;
 	delete m_PluginLoader;
+	m_PluginLoader = NULL;
 	//std::cout << "Switching allocation mode" << std::endl;
 	//delete m_MemoryArena;
 	
 #ifdef _FREYA_DEBUG_MEMORY
 	std::cout << "Engine memory usage at shutdown:\n"
-	<< "\tLeaked in internal manager: " << memory::memory_allocated / 1024.f << " Kb\n"
-	<< "\tTotal number of allocations " << memory::allocation_count << "\n"
-	<< "\tTotal number of deallocations: " << memory::deallocation_count << "\n"
-	<< "\tAlloc - Dealloc: " << (int) memory::alloc_dealloc_dif << "\n"
-	<< "\tSystem memory allocation for internal manager: " << memory::allocated_for_buffers / 1024.f / 1024.f << " Mb" << std::endl;
+			<< "\tLeaked in internal manager: " << memory::memory_allocated / 1024.f << " Kb\n"
+			<< "\tTotal number of allocations " << memory::allocation_count << "\n"
+			<< "\tTotal number of deallocations: " << memory::deallocation_count << "\n"
+			<< "\tAlloc - Dealloc: " << (int) memory::alloc_dealloc_dif << "\n"
+			<< "\tSystem memory allocation for internal manager: " << memory::allocated_for_buffers / 1024.f / 1024.f << " Mb" << std::endl;
 	std::cout << "Engine shutdown completed" << std::endl;
 #endif
 	
@@ -220,15 +298,21 @@ EngineCore::~EngineCore()
 	std::clog.rdbuf(m_OldCLogStream);
 	std::cout.rdbuf(m_OldCOutStream);
 	delete m_OutBuffer;
+	m_OutBuffer = NULL;
 	delete m_LogStringBuf;
+	m_LogStringBuf = NULL;
 	m_LogStream->flush();
 	m_LogStream->close();
 	std::cout << "Destroying log" << std::endl;
 	delete m_LogStream;
+	m_LogStream = NULL;
 
 	delete [] settings::application_name;
+	settings::application_name = NULL;
 	delete [] settings::application_company;
+	settings::application_company = NULL;
 	delete [] settings::config_file_name;
+	settings::config_file_name = NULL;
 
 }
 
@@ -362,7 +446,7 @@ struct __internal_runnable : public EngineSubsystem
 		//std::cout << "Proxy object returned " << retval << std::endl;
 		return retval;
 	}
- 
+
 	core::multithreading::Runnable* run;
 };
 
