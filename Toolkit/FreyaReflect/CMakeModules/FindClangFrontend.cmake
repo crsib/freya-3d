584e238ab9eeccc0e71443157c1dee006148b77b
crsib <crsib@localhost>
2010-11-07 04:42:45 +0000

    Migrating FreyaReflect from ELSA to clang. To simplify build process on UNIX like systems clang/llvm and freyareflect are compiled without STLPort.

---
+++ Toolkit/FreyaReflect/CMakeModules/FindClangFrontend.cmake
@@ -0,0 +1,86 @@
+# FindClangFrontend - Find the clang frontendincludes and library.
+#
+# This module defines
+#  CLANGFRONTEND_INCLUDE_DIR, where to find LLVM/clang headers.
+#  CLANGFRONTEND_LIBRARY_DIR, where to find LLVM/clang libraries.
+#  CLANGFRONTEND_FOUND, If false, do not try to use LLVM/clang.
+#  LLVM_<libname> path to each LLVM static library.
+#  CLANG_<libname> path to each Clang static library.
+#  CLANGFRONTEND_LIBRARIES all libraries to be used in build
+
+if (NOT CLANGFRONTEND_INCLUDE_DIR)
+  find_path(CLANGFRONTEND_INCLUDE_DIR llvm/LinkAllVMCore.h DOC "Path to LLVM headers")
+else (NOT CLANGFRONTEND_INCLUDE_DIR)
+  set(CLANGFRONTEND_INCLUDE_DIR ${CLANGFRONTEND_INCLUDE_DIR} CACHE PATH "Path to LLVM headers")
+endif (NOT CLANGFRONTEND_INCLUDE_DIR)
+
+# Look for the library dir
+if (NOT CLANGFRONTEND_LIBRARY_DIR)
+  find_library(LLVM_CORE LLVMSupport)
+  get_filename_component(CLANGFRONTEND_LIBRARY_DIR ${LLVM_CORE} PATH)
+  set(CLANGFRONTEND_LIBRARY_DIR ${LLVM_LIB_DIR} CACHE PATH "Path to LLVM Libraries")
+  set(LLVM_CORE "")
+endif (NOT CLANGFRONTEND_LIBRARY_DIR)
+
+# Lib macro
+# Find a LLVM libraries
+macro(find_llvm_library Name ReleaseLibName DebugLibName)
+	find_library(LLVM_${Name} ${ReleaseLibName} PATHS ${CLANGFRONTEND_LIBRARY_DIR} NO_DEFAULT_PATH)
+	find_library(LLVM_${Name}_DEBUG ${DebugLibName} PATHS ${CLANGFRONTEND_LIBRARY_DIR} NO_DEFAULT_PATH)
+  
+	if(NOT LLVM_${Name})
+		message( SEND_ERROR "Failed to find ${ReleaseLibName}!" )
+	endif()
+	
+	if(LLVM_${Name}_DEBUG)
+		set( CLANGFRONTEND_LIBRARIES ${CLANGFRONTEND_LIBRARIES} optimized ${LLVM_${Name}} debug ${LLVM_${Name}_DEBUG})
+	else()
+		set( LLVM_${Name}_DEBUG ${LLVM_${Name}})
+		set( CLANGFRONTEND_LIBRARIES ${CLANGFRONTEND_LIBRARIES} ${LLVM_${Name}} )
+	endif()
+	
+	mark_as_advanced(LLVM_${Name})
+	mark_as_advanced(LLVM_${Name}_DEBUG)
+endmacro(find_llvm_library)
+
+# Find clang libraries
+macro(find_clang_library Name ReleaseLibName DebugLibName)
+	find_library(CLANG_${Name} ${ReleaseLibName} PATHS ${CLANGFRONTEND_LIBRARY_DIR} NO_DEFAULT_PATH)
+	find_library(CLANG_${Name}_DEBUG ${DebugLibName} PATHS ${CLANGFRONTEND_LIBRARY_DIR} NO_DEFAULT_PATH)
+  
+	if(NOT CLANG_${Name})
+		message( SEND_ERROR "Failed to find ${ReleaseLibName}!" )
+	endif()
+	
+	if(CLANG_${Name}_DEBUG)
+		set( CLANGFRONTEND_LIBRARIES ${CLANGFRONTEND_LIBRARIES} optimized ${CLANG_${Name}} debug ${CLANG_${Name}_DEBUG})
+	else()
+		set( CLANG_${Name}_DEBUG ${CLANG_${Name}})
+		set( CLANGFRONTEND_LIBRARIES ${CLANGFRONTEND_LIBRARIES} ${CLANG_${Name}} )
+	endif()
+	
+	mark_as_advanced(CLANG_${Name})
+	mark_as_advanced(CLANG_${Name}_DEBUG)
+endmacro(find_clang_library)
+
+find_llvm_library(SYSTEM LLVMSystem LLVMSystem_d)
+find_llvm_library(SUPPORT LLVMSupport LLVMSupport_d)
+find_llvm_library(MC LLVMMC LLVMMC_d)
+find_llvm_library(BITREADER LLVMBitReader LLVMBitReader_d)
+find_llvm_library(BITWRITER LLVMBitWriter LLVMBitWriter_d)
+
+find_clang_library(ANALYSIS clangAnalysis clangAnalysis_d)
+find_clang_library(AST clangAST clangAST_d)
+find_clang_library(BASIC clangBasic clangBasic_d)
+find_clang_library(DRIVER clangDriver clangDriver_d)
+find_clang_library(FRONTEND clangFrontend clangFrontend_d)
+find_clang_library(LEX clangLex clangLex_d)
+find_clang_library(PARSE clangParse clangParse_d)
+find_clang_library(SEMA clangSema clangSema_d)
+
+mark_as_advanced(CLANGFRONTEND_LIBRARIES)
+
+# handle the QUIETLY and REQUIRED arguments and set LLVM_FOUND to TRUE if 
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(CLANGFRONTEND DEFAULT_MSG CLANGFRONTEND_INCLUDE_DIR CLANGFRONTEND_LIBRARY_DIR)
---
+++ Toolkit/FreyaReflect/Driver/version.h
@@ -0,0 +1,8 @@
+#ifndef version_h__
+#define version_h__
+
+#include "SVN_Revision.h"
+
+#define VERSION_STRING "0.1.0.r"##SVN_REVISION_STRING
+
+#endif // version_h__
