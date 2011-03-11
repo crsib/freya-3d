ece743ae757ab1e8cd41c9d98ba88e575248ab49
crsib <crsib@localhost>
2011-03-11 19:55:24 +0300

    Adopting FreyaRelfect to Mercurial

--- Toolkit/FreyaReflect/Driver/version.h
+++ Toolkit/FreyaReflect/Driver/version.h
@@ -1,8 +1,6 @@
 #ifndef version_h__
 #define version_h__
 
-#include "SVN_Revision.h"
-
-#define VERSION_STRING "0.1.3.r"##SVN_REVISION_STRING
+#define VERSION_STRING "0.1.4.hg"
 
 #endif // version_h__
