1e6e0e708acae42503a37d5c0053ba2697c48882
crsib <crsib@localhost>
2011-04-02 22:30:21 +0400

    Cleaning engine file structure due to redesign of engine key futures. TRex library, required  by FreyaSupport is added.

--- FreyaCore/Include/application-settings.h
+++
@@ -1,9 +0,0 @@
-#ifndef APPLICATIONSETTINGS_H_
-#define APPLICATIONSETTINGS_H_
-namespace settings
-{
-	extern char* 		application_name;
-	extern char* 		application_company;
-	extern char*		config_file_name;
-}
-#endif /*APPLICATIONSETTING_H_*/
