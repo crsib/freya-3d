053a97a9dca91339eb327cc7ca75e50ef25f70fa
crsib <crsib@localhost>
2011-04-17 02:07:52 +0400

    vector is mostly implemented.

--- FreyaSupport/Include/containers/details/vector_impl.h
+++ FreyaSupport/Include/containers/details/vector_impl.h
@@ -39,6 +39,64 @@ namespace containers
 		}
 
 		//Copying vector
+		template<typename T>
+		inline
+		T*	__vector_copy_helper(T* first, T* last, T* result, eastl::true_type)
+		{
+			::memcpy(result, first, (last - first) * sizeof(T));
+			return result + (last - first);
+		}
+
+		template<typename T>
+		inline 
+		T* __vector_copy_helper(T* first, T* last, T* result, eastl::false_type)
+		{
+			for( ; first != last; ++first, ++result )
+				::new(result) T(*first);
+			return result;
+		}
+
+		template<typename T>
+		inline
+			T*	__vector_copy_helper( T* result, typename const_reference<T>::type obj, eastl::true_type)
+		{
+			::memcpy(result, &obj, sizeof(T));
+			return result + 1;
+		}
+
+		template<typename T>
+		inline 
+			T* __vector_copy_helper(T* result, typename const_reference<T>::type obj, eastl::false_type)
+		{
+			::new(result) T(obj);
+			return result + 1;
+		}
+
+		template<typename T>
+		inline
+			T*	__vector_copy_backward_helper(T* first, T* last, T* result, eastl::true_type)
+		{
+			::memmove(result, first, (last - first) * sizeof(T));
+			return result + (last - first);
+		}
+
+		template<typename T>
+		inline 
+			T* __vector_copy_backward_helper(T* first, T* last, T* result, eastl::false_type)
+		{
+			const size_t dist = last - first;
+			result += dist;
+			while( last != first )
+				::new(--result) T(*(--last));
+			return result + dist;
+		}
+	}
+
+	template<typename T, template<class> class MAP, class LP, class SRP>
+	vector<T,MAP,LP,SRP>::vector(iterator _begin, iterator _end) 
+		: m_AllocatedCount(get_vector_size(_end - _begin, 0)), m_Begin(allocate(m_AllocatedCount))
+	{
+		m_End = details::__vector_copy_helper(_begin,_end, m_Begin,eastl::has_trivial_copy<T>());
 	}
 
 	template<typename T, template<class> class MAP, class LP, class SRP>
@@ -46,9 +104,187 @@ namespace containers
 	{
 		if( m_Begin != m_End )
 			details::__vector_impl_destructor_helper(m_Begin, m_End, eastl::has_trivial_destructor<T>());
+		// deallocate is expected to act as free
 		deallocate( m_Begin );
+		m_Begin = m_End = NULL;
+		m_AllocatedCount = 0;
 	}
 
+	template<typename T, template<class> class MAP, class LP, class SRP>
+	void vector<T,MAP, LP, SRP>::reset() 
+	{
+		if( m_Begin != m_End )
+			details::__vector_impl_destructor_helper(m_Begin, m_End, eastl::has_trivial_destructor<T>());
+		deallocate( m_Begin );
+		m_Begin = m_End = NULL;
+		m_AllocatedCount = 0;
+	}
+
+	template<typename T, template<class> class MAP, class LP, class SRP>
+	void vector<T,MAP, LP, SRP>::clear() 
+	{
+		if( m_Begin != m_End )
+			details::__vector_impl_destructor_helper(m_Begin, m_End, eastl::has_trivial_destructor<T>());
+	}
+
+	template
+		<
+		typename T,
+		template<class> class MemoryAllocationPolicy,
+	class LockPolicy,
+	class StorageResizePolicy
+		>
+		void containers::vector<T, MemoryAllocationPolicy, LockPolicy, StorageResizePolicy>::erase( iterator first, iterator last )
+	{
+
+	}
+
+	template
+		<
+		typename T,
+		template<class> class MemoryAllocationPolicy,
+	class LockPolicy,
+	class StorageResizePolicy
+		>
+		void containers::vector<T, MemoryAllocationPolicy, LockPolicy, StorageResizePolicy>::erase( iterator position )
+	{
+
+	}
+
+	template
+		<
+		typename T,
+		template<class> class MemoryAllocationPolicy,
+	class LockPolicy,
+	class StorageResizePolicy
+		>
+		void containers::vector<T, MemoryAllocationPolicy, LockPolicy, StorageResizePolicy>::insert( iterator position, const_iterator first, const_iterator last )
+	{
+
+	}
+
+	template
+		<
+		typename T,
+		template<class> class MemoryAllocationPolicy,
+	class LockPolicy,
+	class StorageResizePolicy
+		>
+		void containers::vector<T, MemoryAllocationPolicy, LockPolicy, StorageResizePolicy>::insert( iterator position, size_t n, constant_refernce obj )
+	{
+
+	}
+
+	template
+		<
+		typename T,
+		template<class> class MemoryAllocationPolicy,
+	class LockPolicy,
+	class StorageResizePolicy
+		>
+		void containers::vector<T, MemoryAllocationPolicy, LockPolicy, StorageResizePolicy>::insert( iterator position, constant_refernce obj )
+	{
+		lock();
+		if(!m_Begin)
+		{
+			m_AllocatedCount = get_vector_size(1, 0);
+			m_Begin = allocate(m_AllocatedCount);
+			m_End = m_Begin;
+		}
+		else if( (m_End - m_Begin) == m_AllocatedCount )
+		{
+			const size_t current_size = m_End - m_Begin;
+			const size_t new_size = get_vector_size(current_size + 1, m_AllocatedCount);
+			T*	new_memory_ptr = allocate(new_size);
+			if(position <= m_Begin)
+			{
+				details::__vector_copy_helper(new_memory_ptr, obj, eastl::has_trivial_copy<T>());
+				m_End = details::__vector_copy_helper(m_Begin, m_End, new_memory_ptr + 1, eastl::has_trivial_copy<T>());
+			}
+			else if(position >= m_End)
+			{
+				m_End = details::__vector_copy_helper(m_Begin, m_End, new_memory_ptr, eastl::has_trivial_copy<T>());
+				m_End = details::__vector_copy_helper(m_End, obj, eastl::has_trivial_copy<T>());
+			}
+			else //Worst case
+			{
+				T* mem = details::__vector_copy_helper(m_Begin, position, new_memory_ptr, eastl::has_trivial_copy<T>());
+				mem = details::__vector_copy_helper(mem, obj, eastl::has_trivial_copy<T>());
+				m_End = details::__vector_copy_helper(position, m_End, mem, eastl::has_trivial_copy<T>());
+			}
+			//m_End = details::__vector_copy_helper(m_Begin, m_End, new_memory_ptr, eastl::has_trivial_copy<T>());
+			deallocate(m_Begin);
+			m_Begin = new_memory_ptr;
+		}
+		else
+		{
+			if(position <= m_Begin)
+			{
+				m_End = details::__vector_copy_backward_helper(m_Begin, m_End, m_Begin + 1, eastl::has_trivial_copy<T>());
+				details::__vector_copy_helper(m_Begin, obj, eastl::has_trivial_copy<T>());
+			}
+			else if(position >= m_End)
+			{
+				m_End = details::__vector_copy_helper(m_End, obj, eastl::has_trivial_copy<T>());
+			}
+			else //Worst case
+			{
+				m_End = details::__vector_copy_backward_helper(position, m_End, position + 1, eastl::has_trivial_copy<T>());
+				details::__vector_copy_helper(position, obj, eastl::has_trivial_copy<T>());
+			}
+		}
+		unlock();
+	}
+
+	template
+		<
+		typename T,
+		template<class> class MemoryAllocationPolicy,
+	class LockPolicy,
+	class StorageResizePolicy
+		>
+		void containers::vector<T, MemoryAllocationPolicy, LockPolicy, StorageResizePolicy>::pop_back()
+	{
+		lock();
+		if(m_Begin && m_Begin < m_End)
+		{
+			details::__vector_impl_destructor_helper(m_End - 1, m_End, eastl::has_trivial_destructor<T>());
+			--m_End;
+		}
+		unlock();
+	}
+
+	template
+		<
+		typename T,
+		template<class> class MemoryAllocationPolicy,
+	class LockPolicy,
+	class StorageResizePolicy
+		>
+		void containers::vector<T, MemoryAllocationPolicy, LockPolicy, StorageResizePolicy>::push_back( constant_refernce obj )
+	{
+		lock();
+		if(!m_Begin)
+		{
+			m_AllocatedCount = get_vector_size(1, 0);
+			m_Begin = allocate(m_AllocatedCount);
+			m_End = m_Begin;
+		}
+		else if( (m_End - m_Begin) == m_AllocatedCount )
+		{
+			//Wtf, copying...
+			const size_t current_size = m_End - m_Begin;
+			const size_t new_size = get_vector_size(current_size + 1, m_AllocatedCount);
+			T*	new_memory_ptr = allocate(new_size);
+			m_End = details::__vector_copy_helper(m_Begin, m_End, new_memory_ptr, eastl::has_trivial_copy<T>());
+			deallocate(m_Begin);
+			m_Begin = new_memory_ptr;
+		}
+
+		m_End = details::__vector_copy_helper(m_End, obj, eastl::has_trivial_copy<T>());
+
+		unlock();
+	}
 
 }
 
--- FreyaSupport/Include/containers/vector.h
+++ FreyaSupport/Include/containers/vector.h
@@ -9,6 +9,7 @@
 #define vector_h__
 
 #include "containers/policies/Policies.h"
+#include "containers/type_utils.h"
 
 namespace containers
 {
@@ -23,22 +24,161 @@ namespace containers
 		: private MemoryAllocationPolicy<T>, private LockPolicy, private StorageResizePolicy
 	{
 	public:
+		//! Vector iterator type
+		typedef T*		iterator;
+		//! Vector const iterator type
+		typedef typename const_pointer<T>::type const_iterator;
+		//! Vector base type
+		typedef T		type;
+		//! Reference to base type
+		typedef T&		reference;
+		//! Constant reference type
+		typedef typename const_reference<T>::type constant_refernce;
+		//! Reverse iterator implementation
+		/*!
+		 * This a wrapper around an vector iterator, which allows reverse iteration through the container
+		 * It emulates the normal iterator behavior (a pointer, in this case)
+		 */
+		template<typename T>
+		class reverse_iterator_impl
+		{
+			template<typename U> 
+			friend class reverse_iterator_impl;
+			T*	m_Ptr;
+			reverse_iterator_impl () {}
+		public:
+			explicit reverse_iterator_impl(T* ptr) { m_Ptr = ptr; }
+			reverse_iterator_impl(const reverse_iterator_impl& other) : m_Ptr(other.m_Ptr) {}
+			//Requires implicit cast
+			template<typename U>
+			reverse_iterator_impl(const reverse_iterator_impl<U>& other) : m_Ptr(other.m_Ptr) {}
+			reverse_iterator_impl& operator = (const reverse_iterator_impl& rhs) { m_Ptr = rhs.m_Ptr; return *this;}
+
+			reverse_iterator_impl& operator ++ () { --m_Ptr; return *this; }
+			reverse_iterator_impl  operator ++ (int) { return reverse_iterator_impl(m_Ptr--); }
+
+			reverse_iterator_impl& operator -- () { ++m_Ptr; return *this; }
+			reverse_iterator_impl  operator -- (int) { return reverse_iterator_impl(m_Ptr++); }
+
+			reverse_iterator_impl  operator + (size_t offset) const { return reverse_iterator_impl(m_Ptr - offset); }
+			reverse_iterator_impl  operator - (size_t offset) const { return reverse_iterator_impl(m_Ptr + offset); }
+			ptrdiff_t  operator - (reverse_iterator_impl const& offset) const { return reverse_iterator_impl(offset.m_Ptr - m_Ptr); }
+
+			reverse_iterator_impl&  operator += (size_t offset) { return *this = reverse_iterator_impl(m_Ptr - offset); }
+			reverse_iterator_impl&  operator -= (size_t offset) { return *this = reverse_iterator_impl(m_Ptr + offset); }
+
+			T&	operator * () { return *m_Ptr; }
+			typename const_reference<T>::type
+				operator * () const { return *m_Ptr; }
+			T*	operator -> () { return m_Ptr; }
+			typename const_pointer<T>::type
+				operator -> () const { return m_Ptr; }
+
+			friend
+				inline bool operator == (const reverse_iterator_impl& lhs, const reverse_iterator_impl& rhs) { return lhs.m_Ptr == rhs.m_Ptr; }
+			friend
+				inline bool operator != (const reverse_iterator_impl& lhs, const reverse_iterator_impl& rhs) { return lhs.m_Ptr != rhs.m_Ptr; }
+
+		};
+		//! Reverse iterator
+		typedef reverse_iterator_impl<T>	reverse_iterator;
+		//! Constant reverse iterator
+		typedef reverse_iterator_impl<typename constant<T>::type> const_reverse_iterator;
 		//! Destroy a vector
 		~vector();
 		//! Constructs an empty vector
 		vector() : m_AllocatedCount(0),  m_Begin(NULL), m_End(NULL) {}
 		//! Construct an empty vector with reserved storage
-		vector(size_t reserve_count) : m_AllocatedCount(get_vector_size(reserve_count, 0)), m_Begin(allocate(reserve_count)), m_End(m_Begin) {}
+		vector(size_t reserve_count) 
+			: m_AllocatedCount(get_vector_size(reserve_count, 0)), m_Begin(allocate(reserve_count)), m_End(m_Begin) {}
+		//! Construct a vector initialized with [_begin, _end)
+		vector(iterator _begin, iterator _end); 
 
+		//Accessing vector data
+		//! Overloaded operator []
+		reference	operator [] (size_t idx) 
+		{
+			FREYA_SUPPORT_ASSERT(m_Begin, "Empty vector");
+			FREYA_SUPPORT_ASSERT(idx < static_cast<size_t>(m_End - m_Begin), "Out of range");
+			return *(m_Begin + idx);
+		}
+		//! Overloaded operator [] (const version)
+		constant_refernce operator [] (size_t idx) const
+		{
+			FREYA_SUPPORT_ASSERT(m_Begin, "Empty vector");
+			FREYA_SUPPORT_ASSERT(idx < static_cast<size_t>(m_End - m_Begin), "Out of range");
+			return *(m_Begin + idx);
+		}
 
+		//! 
+		iterator		begin() { return m_Begin; }
+		//! 
+		iterator		end() { return m_End; }
+		//!
+		const_iterator	begin() const { return m_Begin; }
+		//!
+		const_iterator  end() const { return m_End; }
+		//! 
+		reverse_iterator rbegin() { FREYA_SUPPORT_ASSERT(m_End,"Empty vector"); return reverse_iterator(m_End - 1); }
+		//!
+		reverse_iterator rend() { FREYA_SUPPORT_ASSERT(m_Begin,"Empty vector"); return reverse_iterator(m_Begin - 1);}
+		//! 
+		const_reverse_iterator rbegin() const { FREYA_SUPPORT_ASSERT(m_End,"Empty vector"); return const_reverse_iterator(m_End - 1); }
+		//!
+		const_reverse_iterator rend() const { FREYA_SUPPORT_ASSERT(m_Begin,"Empty vector"); return const_reverse_iterator(m_Begin - 1);}
+		//!
+		reference			   front() { FREYA_SUPPORT_ASSERT(m_Begin < m_End,"Empty vector"); return *m_Begin; }
+		//!
+		reference			   back()  { FREYA_SUPPORT_ASSERT(m_Begin < m_End,"Empty vector"); return *(m_End-1); }	
+		//!
+		constant_refernce	   front() const { FREYA_SUPPORT_ASSERT(m_Begin < m_End,"Empty vector"); return *m_Begin; }
+		//!
+		constant_refernce	   back() const { FREYA_SUPPORT_ASSERT(m_Begin < m_End,"Empty vector"); return *(m_End-1); }	
 		//! Get reserved space size
 		size_t	capacity() const { return m_AllocatedCount; }
 		//! Get reserved space size in bytes
 		size_t	storage_size() const { return m_AllocatedCount * sizeof(T); }
+		//! Check, if vector is empty
+		bool	empty() const { return m_Begin == m_End; }
+		//! Get the number of elements in the vector.
+		size_t	size() const { return m_End - m_Begin; }
+
+		// Manipulating the vector
+		//! Push an item to vector
+		void	push_back(constant_refernce	obj);
+		//!Pop an item  from the vector
+		void	pop_back();
+
+		//! Insert an item into vector
+		void	insert(iterator	position, constant_refernce	obj );
+		//! Insert n copies of item into vector
+		void	insert(iterator	position, size_t n, constant_refernce	obj );
+		//! Insert range of items into the vector
+		void	insert(iterator	position, const_iterator first, const_iterator last);
+
+		//!
+		void	erase( iterator position );
+		//!
+		void	erase( iterator first, iterator last );
+		//! Reset the vector
+		/*!
+		 * Clean up the memory, used by the vector and delete all vector elements
+		 */
+		void	reset();
+		//! Clear the vector
+		/*
+		 * Destroy all objects within vector without deallocating memory
+		 */
+		void	clear();
 	private:
 		size_t	m_AllocatedCount;
 		T*	   m_Begin;
 		T*	   m_End;
+
+		//Have no idea, why would someone create a deep copy of an vector itself, 
+		//especially considering problem of copying vector with different policies
+		vector(const vector& v);
+		vector& operator = (const vector& v);
 	};
 }
 
