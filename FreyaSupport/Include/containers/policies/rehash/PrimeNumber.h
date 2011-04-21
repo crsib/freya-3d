ee9ec909f4aeae9f6e65ac45734a90c07c11e88e
crsib <crsib@localhost>
2011-04-21 12:44:06 +0400

    PrimeNumber policy implemented

--- FreyaSupport/Include/containers/policies/rehash/PrimeNumber.h
+++ FreyaSupport/Include/containers/policies/rehash/PrimeNumber.h
@@ -8,5 +8,35 @@
 #ifndef Containers_PrimeNumber_h__
 #define Containers_PrimeNumber_h__
 
+#include "integer.h"
+#include "FreyaSupportInternal.h"
+
+#include <cmath>
+
+namespace containers
+{
+	namespace policies
+	{
+		namespace rehash
+		{
+			class FREYA_SUPPORT_EXPORT PrimeNumber
+			{
+				float				m_LoadFactor;
+				mutable size_t		m_NextRehashSize; //This parameter is lazy evaluated in get_bucket_count
+			public:
+				explicit PrimeNumber(float load_factor)  
+					: m_LoadFactor(load_factor) , m_NextRehashSize( static_cast<uint32_t>( floorf( m_LoadFactor * 11 ) ) ) {}
+				bool	rehash_needed(size_t elem_count, size_t bucket_count) const 
+				{
+					if(elem_count <= m_NextRehashSize)
+						return false;
+					return (static_cast<float>(elem_count) / bucket_count) > m_LoadFactor;
+				}
+
+				uint32_t get_bucket_count( size_t elem, size_t bucket_count ) const;
+			};
+		}
+	}
+}
 
 #endif // PrimeNumber_h__
