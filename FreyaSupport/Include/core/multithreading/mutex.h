afdf2ae25f014238dfdcfaf73cf2a04e8447bf86
vitalij <vitalij@localhost>
2011-04-26 19:00:52 +0400

    Added implementation for mutex class (win32 platform).

--- FreyaSupport/Include/core/multithreading/mutex.h
+++ FreyaSupport/Include/core/multithreading/mutex.h
@@ -15,19 +15,60 @@ namespace core
 {
 	namespace multithreading
 	{
-		class FREYA_SUPPORT_EXPORT mutex : public details::mutex_rep
+		/// \brief This class provides basic operations on mutex objects.
+		/** Mutex instances are not copiable. Mutex can not be locked
+		 * recursievly by the owner thread. */
+		class FREYA_SUPPORT_EXPORT mutex : private details::mutex_rep
 		{
 		public:
+			/// \brief Creates mutex object. Use mutex::is_valid() to check that object was properly created.
 			inline mutex();
+
+			/// \brief Destroy mutex object.
 			inline ~mutex();
 
+			/// \brief Retrieve state of the object.
+			/** Normaly it should always return positive value. 
+			  * \return Boolean value. True if object is valid. */
+			inline bool is_valid() const;
+
+			/// \brief Locks the mutex.
+			/** If the mutex object was locked by some other thread before, execution
+			 * of calling thread will be stoped until mutex object will be released.
+			 * \return Boolean value. Positive if mutex was locked, negative in case of
+			 * error.
+			 */
 			inline bool lock();
-			inline bool lock(const unsigned ms);
+
+			///brief Locks the mutex (timed version).
+			/** If the mutex object was locked by some other thread before, execution
+			 * of calling thread will be stoped until mutex object will be released or
+			 * waiting time have been elapsed.
+			 * \param timeout Time to wait in milliseconds.
+			 * \return Boolean value. Positive if mutex was locked, negative in case of
+			 * error or timeout elapse.
+			 */
+			inline bool lock(const unsigned timeout);\
+			
+			/// \brief Tries to lock the mutex object.
+			/** If the mutex object was locked by some other thread before, calling
+			 * thread will continue it's execution immediately.
+			 * \return Boolead value. True if mutex was locked, false in case of 
+			 * error or mutex was locked.
+			 */
 			inline bool try_lock();
-			inline void release();
+			
+			/// \brief Release the mutex object.
+			/** Operation complimentary to lock. Mutex can be released only by the
+			 * thread owned it before.
+			 * \return Boolean value. True on success, false in case of error.
+			 */
+			inline bool release();
 
 		private:
+			// Copy constructor is forbidden.
 			mutex(const mutex&);
+			// Assignment operator if forbidden.
 			mutex& operator=(const mutex&);
 		};
 	}//namespace multithreading
--- FreyaSupport/Include/core/multithreading/win32/mutex.h
+++ FreyaSupport/Include/core/multithreading/win32/mutex.h
@@ -8,4 +8,53 @@
 #ifndef FREYA_PLATFORM_WIN_MUTEX_H_
 #define FREYA_PLATFORM_WIN_MUTEX_H_
 
+#include "core/multithreading/details/platform.h"
+#include "core/multithreading/mutex.h"
+
+namespace core
+{
+	namespace multithreading
+	{
+		inline mutex::mutex()
+			: mutex_rep(CreateMutex(NULL, false, NULL))
+		{
+		}
+
+		inline mutex::~mutex()
+		{
+			if(mutex_rep::m_mutex_handle != NULL)
+			{
+				CloseHandle(mutex_rep::m_mutex_handle);
+				mutex_rep::m_mutex_handle = NULL;
+			}
+		}
+
+		inline bool mutex::is_valid() const
+		{
+			return (mutex_rep::m_mutex_handle != NULL);
+		}
+
+		inline bool mutex::lock()
+		{
+			return (WaitForSingleObject(mutex_rep::m_mutex_handle, INFINITE) == WAIT_OBJECT_0);
+		}
+
+		inline bool mutex::lock(const unsigned timeout)
+		{
+			return (WaitForSingleObject(mutex_rep::m_mutex_handle, static_cast<DWORD>(timeout)) == WAIT_OBJECT_0);
+		}
+
+		inline bool mutex::try_lock()
+		{
+			return (WaitForSingleObject(mutex_rep::m_mutex_handle, 0) == WAIT_OBJECT_0);
+		}
+
+		inline bool mutex::release()
+		{
+			return (ReleaseMutex(mutex_rep::m_mutex_handle) == TRUE);
+		}
+
+	}//namespace multithreading
+}//namespace core
+
 #endif//FREYA_PLATFORM_WIN_MUTEX_H_
